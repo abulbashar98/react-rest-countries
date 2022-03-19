0e7654edcd57649df98114514c82cbc95898b62c
abulbashar98 <abulbashar98116@gmail.com>
2022-03-19 18:11:19 +0600

    send-multiple-data-throug-sending-whole-object-in-one-property-use-destructuring-in-child-Component

--- src/Components/countries/Countries.js
+++ src/Components/countries/Countries.js
@@ -14,14 +14,40 @@ function Countries() {
             .then(data => setCountries(data))
     }, [])
 
+
     return (
-        <div>
+        // This is how you send props one by one in the child Component.....
+
+        // {/* <div>
+        //     <h2>All countries in the World From another file</h2>
+        //     <h3>Available: {countries.length}</h3>
+        //     {
+        //         countries.map(country => <Country
+        //             name={country.name}
+        //             capital={country.capital}
+        //             region={country.region}
+        //             population={country.population}
+        //             borders={country.borders}>
+
+        //         </Country>)
+        //     }
+        // </div> */}
+
+
+
+
+        // This is How Send the Whole Object as a property to Child Component.....
+
+        < div >
             <h2>All countries in the World From another file</h2>
             <h3>Available: {countries.length}</h3>
             {
-                countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+                countries.map(country => <Country
+                    country={country}></Country>)
             }
-        </div>
+        </div >
+
+
     )
 }
 
--- src/Components/country/Country.js
+++ src/Components/country/Country.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
 const Country = (props) => {
+    const { flag, name, capital, region, population, borders } = props.country;
+
     return (
-        <div>
-            <div className='imported-style' style={{ backgroundColor: 'cyan', margin: '10px auto', width: '75vw', borderRadius: '25px', padding: '10px', color: 'blue' }}>
-                <h2>Name: {props.name}</h2>
-                <h2>Capital: {props.capital}</h2>
-            </div>
+        // 1. The way to get by one props from the Parent Component.....
+
+        // {/* <div>
+        //     <div className='imported-style' style={{ backgroundColor: 'cyan', margin: '10px auto', width: '75vw', borderRadius: '25px', padding: '10px', color: 'blue' }}>
+        //         <h2>Name: {props.name}</h2>
+        //         <h2>Capital: {props.capital}</h2>
+        //         <h4>Region: {props.region}</h4>
+        //         <h4>Population: {props.population}</h4>
+        //         <li>Borders: {props.borders}</li>
+        //     </div>
+        // </div> */}
+
+
+
+
+        // 2. Recommended way of getting the whole Object from Parent Component and then use it's property one by one.....
+
+        // {/* <div className='imported-style' style={{ backgroundColor: 'cyan', margin: '10px auto', width: '75vw', borderRadius: '25px', padding: '10px', color: 'blue' }}>
+        //     <img style={{ width: '50%' }} src={props.country.flag} alt="" />
+        //     <h2>Name: {props.country.name}</h2>
+        //     <h2>Capital: {props.country.capital}</h2>
+        //     <h4>Region: {props.country.region}</h4>
+        //     <h4>Population: {props.country.population}</h4>
+        //     <li>Borders: {props.country.borders}</li>
+        // </div> */}
+
+
+
+
+        // 3. Best way of Using destructuring Object That was sent by the Parent Component and 
+        //    then use only variables.......
+
+        <div className='imported-style' style={{ backgroundColor: 'cyan', margin: '10px auto', width: '75vw', borderRadius: '25px', padding: '10px', color: 'blue' }}>
+            <img style={{ width: '50%' }} src={flag} alt="" />
+            <h2>Name: {name}</h2>
+            <h2>Capital: {capital}</h2>
+            <h4>Region: {region}</h4>
+            <h4>Population: {population}</h4>
+            <li>Borders: {borders}</li>
         </div>
+
     );
 };
 
